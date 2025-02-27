1cade5ca2bdbc49bb4be014601aa73ba1f5e4be4
Prabin Thapa <thapaprabin071@gmail.com>
2025-02-27 14:17:29 +0545

    background loaded

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include "raylib.h"
+#include<stdio.h>
+
+int main(void)
+{
+    // Initialize the window with a temporary size
+    InitWindow(800, 800, "dsa-queue-simulator");
+
+    if (FileExists("Images/astroid.png")) {
+        printf("File found!\n");
+    } else {
+        printf("File not found!\n");
+        return 1;  // Exit if the file is not found
+    }
+    
+
+    Texture2D background = LoadTexture("Images/img.png");
+
+    if (background.id == 0) {
+        printf("Failed to load texture img.jpg\n");
+        return 1;  // Exit if the texture fails
+    }
+    else {
+        printf("Texture loaded successfully\n");
+    }
+    
+    // Switch to fullscreen
+    //ToggleFullscreen();
+
+    while (!WindowShouldClose()) // Main loop
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+         DrawTexturePro(background, 
+            (Rectangle){0, 0, background.width, background.height}, 
+            (Rectangle){0, 0, 800, 800}, 
+            (Vector2){0, 0}, 0.0f, WHITE);
+
+        DrawText("assignment 1",0,0, 20, WHITE);
+        EndDrawing();
+    }
+
+    UnloadTexture(background);
+
+    CloseWindow(); // Cleanup
+    return 0;
+}
